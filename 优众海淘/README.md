7ebc854983b8bf164534eccc6418e2a0f8201d58
EaseMobIMDemo <425035106@qq.com>
2015-06-16 14:57:56 +0800

    #update

--- 优众海淘/README.md
+++ 优众海淘/README.md
@@ -1,1 +1,36 @@
-Cloud code project for 优众海淘. Cloud Code Guide: https://cn.avoscloud.com/docs/cloud_code_guide.html
+# LeanChat 服务端
+
+## 简介
+
+LeanChat 是 [LeanCloud](http://leancloud.cn) [实时通信](https://leancloud.cn/docs/realtime.html) 组件的 Demo，通过该应用你可以学习和了解 LeanCloud 实时通信功能。
+
+应用体验下载地址：[http://fir.im/leanchat](http://fir.im/leanchat)
+
+## 验证签名算法
+签名逻辑可能实现在了您的服务器上，没有用 LeanCloud 的 LeanEngine 功能，可以前往 https://leanchat.avosapps.com/convSign 来校验签名算法是否正确。
+
+## Leanchat 项目构成
+
+* [Leanchat-android](https://github.com/leancloud/leanchat-android)：Android 客户端
+* [Leanchat-ios](https://github.com/leancloud/leanchat-ios)：iOS 客户端
+* [Leanchat-cloud-code](https://github.com/leancloud/leanchat-cloudcode)：可选服务端，使用 LeanCloud [云代码](https://leancloud.cn/docs/cloud_code_guide.html) 实现，实现了聊天的签名，更安全。
+
+
+## 部署服务端
+
+1. fork
+2. 管理台在云代码相关位置填写地址
+3. 管理台点击部署
+
+## 文档
+
+* git 仓库部署：[相关文档](https://leancloud.cn/docs/cloud_code_guide.html#部署代码)
+* 命令行工具部署：[相关文档](https://leancloud.cn/docs/cloud_code_commandline.html#部署)
+
+## 开发相关
+
+### 相关接口
+
+* `conv_sign`：对聊天操作进行签名
+
+代码详见 [main.js](https://github.com/leancloud/AdventureCloud/blob/master/cloud/main.js)
--- 优众海淘/cloud/app.js
+++ 优众海淘/cloud/app.js
@@ -1,16 +1,95 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
+var muser = require('cloud/muser');
+var mutil = require('cloud/mutil');
+var _ = require('underscore');
+var msign = require('cloud/msign');
+var mchat = require('cloud/mchat');
+var mlog = require('cloud/mlog');
 
 // App 全局配置
-app.set('views','cloud/views');   // 设置模板目录
+app.set('views', 'cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
+function setUserAvatar(req, res) {
+  muser.findAllUsers().then(function (users) {
+    mutil.findAll('Avatar').then(function (avatars) {
+      var ps = [];
+      _.each(users, function (user) {
+        var len = avatars.length;
+        var pos = Math.floor(len * Math.random());
+        if (user.get('avatar') == null) {
+          user.set('avatar', avatars[pos].get('file'));
+          ps.push(user.save());
+        }
+      });
+      AV.Promise.when(ps).then(function () {
+        res.send('ok');
+      }, mutil.renderErrorFn(res));
+    });
+  }, mutil.renderErrorFn(res));
+}
 
-// 最后，必须有这行代码来使 express 响应 HTTP 请求
+function handlePromise(p, res) {
+  p.then(function () {
+    res.send('ok');
+  }, mutil.renderErrorFn(res));
+}
+
+function convSignTest(req, res) {
+  //open
+  msign._convSign("selfId", null, null, null);
+  //start
+  msign._convSign("selfId", null, ["t1", "t2"], null);
+  //add
+  msign._convSign("selfId", "convid", ["t1", "t2"], "invite");
+  //kick
+  var result = msign._convSign("selfId", "convid", ["t1", "t2"], "kick");
+  res.send(result);
+}
+
+function convSign(req, res) {
+  var appId = req.query.appId;
+  var masterKey = req.query.masterKey;
+  var selfId = req.query.selfId;
+  var convid = req.query.convid;
+  var targetIds = req.query.targetIds;
+  if (targetIds) {
+    targetIds = targetIds.split(",");
+  }
+  var action =  req.query.action;
+  var nonce = req.query.nonce;
+  var timestamp = req.query.timestamp;
+  var result = msign._convSign(selfId, convid, targetIds, action, appId, masterKey, nonce, timestamp);
+  res.render('main',{result:JSON.stringify(result)});
+}
+
+function renderMainPage(req, res) {
+   res.render('main');
+}
+
+function pushMessageTest(req, res) {
+  var params = {content: '{"_lctype":-1,"_lctext":"sdfsdfsdf"}',
+    fromPeer: '544f2a25e4b0e9dff2e9b272', offlinePeers: ['5416d9b2e4b0f645f29ddbfd'],
+    conversationId: 'id'};
+  mchat._receiversOffLine(params).then(function (result) {
+    res.send(result);
+  });
+}
+
+function test(req, res) {
+  res.send('ok');
+}
+
+if (__production == false) {
+  app.get('/setAvatars', setUserAvatar);
+  app.get('/convSignTest', convSignTest);
+  app.get('/pushMessageTest', pushMessageTest);
+  app.get('/test', test);
+}
+
+app.get('/convSign',convSign);
+
 app.listen();
